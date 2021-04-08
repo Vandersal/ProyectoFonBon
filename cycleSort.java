0e3579e00109fbcb1a993d9c225c160e542b58c3
VandersalAheram <201224@ids.upchiapas.edu.mx>
2021-04-08 12:22:46 -0500

    Actualización de cycle sort

--- cycleSort.java
+++ cycleSort.java
@@ -1,75 +1,50 @@
-// Java program to implement cycle sort
-
-import java.util.*;
-import java.lang.*;
-
-class GFG {
-    // Function sort the array using Cycle sort
-    public static void cycleSort(int arr[], int n) {
-        // count number of memory writes
-        int writes = 0;
-
-        // traverse array elements and put it to on
-        // the right place
-        for (int cycle_start = 0; cycle_start <= n - 2; cycle_start++) {
-            // initialize item as starting point
-            int item = arr[cycle_start];
-
-            // Find position where we put the item. We basically
-            // count all smaller elements on right side of item.
-            int pos = cycle_start;
-            for (int i = cycle_start + 1; i < n; i++)
-                if (arr[i] < item)
-                    pos++;
-
-            // If item is already in correct position
-            if (pos == cycle_start)
-                continue;
-
-            // ignore all duplicate elements
-            while (item == arr[pos])
-                pos += 1;
-
-            // put the item to it's right position
-            if (pos != cycle_start) {
-                int temp = item;
-                item = arr[pos];
-                arr[pos] = temp;
-                writes++;
-            }
-
-            // Rotate rest of the cycle
-            while (pos != cycle_start) {
-                pos = cycle_start;
-
-                // Find position where we put the element
-                for (int i = cycle_start + 1; i < n; i++)
-                    if (arr[i] < item)
-                        pos += 1;
-
-                // ignore all duplicate elements
-                while (item == arr[pos])
-                    pos += 1;
-
-                // put the item to it's right position
-                if (item != arr[pos]) {
-                    int temp = item;
-                    item = arr[pos];
-                    arr[pos] = temp;
-                    writes++;
-                }
-            }
-        }
-    }
-
-    // Driver program to test above function
-    public static void main(String[] args) {
-        int arr[] = { 1, 8, 3, 9, 10, 10, 2, 4 };
-        int n = arr.length;
-        cycleSort(arr, n);
-
-        System.out.println("After sort : ");
-        for (int i = 0; i < n; i++)
-            System.out.print(arr[i] + " ");
-    }
-}
+public class cycleSort   
+{  
+public static void sort(int a[], int n)  
+{  
+    int writes = 0,start,element,pos,temp,i;  
+   
+    for (start = 0; start <= n - 2; start++) {  
+        element = a[start];  
+        pos = start;  
+        for (i = start + 1; i < n; i++)  
+            if (a[i] < element)  
+                pos++;  
+        if (pos == start)  
+            continue;  
+        while (element == a[pos])  
+            pos += 1;  
+        if (pos != start) {  
+            //swap(element, a[pos]);  
+        temp = element;  
+        element = a[pos];  
+            a[pos] = temp;    
+            writes++;  
+        }  
+        while (pos != start) {  
+            pos = start;  
+            for (i = start + 1; i < n; i++)  
+                if (a[i] < element)  
+                    pos += 1;  
+            while (element == a[pos])  
+                pos += 1;  
+            if (element != a[pos]) {  
+              temp = element;  
+          element = a[pos];  
+              a[pos] = temp;    
+                writes++;  
+            }  
+        }  
+    }  
+}  
+public static void main(String[] args)  
+{  
+    int a[] = { 1, 9, 2, 4, 2, 10, 45, 3, 2 };  
+    int n = a.length,i;  
+    sort(a, n);  
+    System.out.println("After sort, array : ");  
+    for (i = 0; i < n; i++)  
+        System.out.println(a[i]);  
+      
+}  
+}  
