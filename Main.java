7d4dae6a0b54a5d4ece380f4113dc21bee5f3554
luis molina <55844181+luixito@users.noreply.github.com>
2021-04-08 12:11:39 -0500

    Implementacion del fisherYates
    Implementación de FisherYates en el main del programa

--- Main.java
+++ Main.java
@@ -1,24 +1,73 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.Random;
 
 public class Main {
-    static ArrayList<Datos> datos = new ArrayList<Datos>();
 
     public static void main(String[] args) {
-
+        ArrayList<Datos> datos = new ArrayList<Datos>();
         setDatos(datos);
 
-        for (int x = 0; x < datos.size(); x++)
+        for (int x = 0; x < datos.size(); x++) {
             System.out.println(datos.get(x).toString());
+        }
+
+        randomize(datos);
     }
 
-        //a Vandersal le gusta el pene
-        
     public static void setDatos(ArrayList<Datos> datos) {
         Random r = new Random();
-        int valorRandom = r.nextInt(10000);
+        for (int x = 0; x < 10; x++) {
+            int valorRandom = r.nextInt(10000);
 
-        Datos ejemplo = new Datos(valorRandom, "a");
-        datos.add(ejemplo);
+            Datos ejemplo = new Datos(valorRandom, "a");
+            datos.add(ejemplo);
+        }
+    }
+
+    static void randomize(ArrayList arrayList) {
+        // creating
+        // ArrayList
+        ArrayList<Integer> al = new ArrayList<Integer>();
+
+        // adding object in ArrayList
+        al.add(10);
+        al.add(20);
+        al.add(30);
+        al.add(40);
+        al.add(50);
+        al.add(60);
+        al.add(70);
+        al.add(80);
+
+        System.out.println("Before shuffling Arraylist:");
+
+        // getting Iterator
+        // from arraylist to
+        // traverse elements
+        Iterator itr = arrayList.iterator();
+
+        while (itr.hasNext()) {
+            System.out.println(itr.next() + " ");
+        }
+
+        System.out.println("");
+
+        Random r1 = new Random();
+
+        for (int i = arrayList.size() - 1; i >= 1; i--) {
+            // swapping current index value
+            // with random index value
+            Collections.swap(arrayList, i, r1.nextInt(i + 1));
+        }
+
+        System.out.println("After shuffling Arraylist:");
+
+        itr = arrayList.iterator();
+
+        while (itr.hasNext()) {
+            System.out.println(itr.next() + " ");
+        }
     }
 }
