947c039126b6a3c88c4409a9d4087a91bf018f53
luis molina <55844181+luixito@users.noreply.github.com>
2021-04-10 16:35:44 -0500

    Implementación de ordenamiento por alfabeto...
    Se implemento el ordenamiento por alfabeto sin afectar el previo ordenamiento por numero. se corrijo el error al llenar el array de tipo "Datos" que daba cuando "y.random" daba como resultado 27.

--- Main.java
+++ Main.java
@@ -1,55 +1,55 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
+        // creacion del arraylist de tipo datos y llenado del alfabeto
         ArrayList<Datos> datos = new ArrayList<Datos>();
-        setDatos(datos);
+        String[] arrayLetras = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+                "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+        setDatos(datos, arrayLetras);
 
         for (int x = 0; x < datos.size(); x++) {
             System.out.println(datos.get(x).toString());
         }
 
-        // fisherYates(datos);
+        fisherYates(datos);
         // cycleSort(datos, datos.size());
-
-        for (int x = 0; x < datos.size(); x++) {
-            System.out.println(datos.get(x).toString());
-        }
 
     }
 
-    public static void setDatos(ArrayList<Datos> datos) {
-        String[] arrayLetras = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
-                "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+    public static void setDatos(ArrayList<Datos> datos, String[] arrayLetras) {
+        // llenado de arraylist de tipo Datos
         Random r = new Random();
         Random y = new Random();
         for (int x = 0; x < 10; x++) {
             int valorRandom = r.nextInt(10000);
-            int randomLetra = y.nextInt(27);
+            int randomLetra = y.nextInt(26);
 
-            Datos ejemplo = new Datos(valorRandom, arrayLetras[randomLetra]);
-            datos.add(ejemplo);
+            Datos contenidDatos = new Datos(valorRandom, arrayLetras[randomLetra]);
+            datos.add(contenidDatos);
         }
     }
 
     static void fisherYates(ArrayList<Datos> arrayList) {
         Iterator itr = arrayList.iterator();
-        System.out.println("");
-
         Random r1 = new Random();
 
+        // intercambio de lugares del contenido del array
         for (int i = arrayList.size() - 1; i >= 1; i--) {
             Collections.swap(arrayList, i, r1.nextInt(i + 1));
         }
 
-        System.out.println("After shuffling Arraylist:");
+        System.out.println("");
 
         itr = arrayList.iterator();
 
+        // imprecion del array desordenado
         while (itr.hasNext()) {
             System.out.println(itr.next() + " ");
         }
@@ -71,7 +71,6 @@ public class Main {
             while (element.getNumero() == a.get(pos).getNumero())
                 pos += 1;
             if (pos != start) {
-                // swap(element, a[pos]);
                 temp = element;
                 element = a.get(pos);
                 a.set(pos, temp);
@@ -92,6 +91,20 @@ public class Main {
                 }
             }
         }
+
+        // ordenador alfabetico
+        Collections.sort(a, new Comparator<Datos>() {
+            public int compare(Datos obj1, Datos obj2) {
+                return obj1.getLetra().compareTo(obj2.getLetra());
+            }
+        });
+
+        // imprecion del array ordenado
+        System.out.println("");
+        for (int x = 0; x < a.size(); x++) {
+            System.out.println(a.get(x).toString());
+        }
+
     }
 
-    
+}
